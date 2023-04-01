56f7ff5a4dd37c3d6b3bf1551cf44ace51a83276
fountesh <128893715+fountesh@users.noreply.github.com>
2023-04-01 11:45:09 +0300

    Delete main directory

--- main/фото редактор/photo_red.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image, ImageFilter 
-with Image.open("фото редактор\Обои с темы.jpg") as original:
-    new_image = original.convert("L")
-    new_image = new_image.filter(ImageFilter.BLUR)
-    new_image = new_image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
-    new_image = new_image.transpose(Image.Transpose.ROTATE_90)
-    new_image.show()
